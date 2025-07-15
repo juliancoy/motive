ac6ec713571f3bca5edba0bc77fa044e9409ad8d
Julian <julian@codecollective.us>
2025-07-15 13:26:13 -0400

    more better texture handling

--- motive/model.cpp
+++ motive/model.cpp
@@ -50,7 +50,7 @@ Primitive::Primitive(Engine *engine, Mesh *mesh, const std::vector<Vertex> &vert
       uniformBuffer(VK_NULL_HANDLE),
       uniformBufferMemory(VK_NULL_HANDLE),
       uniformBufferMapped(nullptr),
-      descriptorSet(VK_NULL_HANDLE)
+      primitiveDescriptorSet(VK_NULL_HANDLE)
 {
     // Create vertex buffer
     VkDeviceSize bufferSize = sizeof(vertices[0]) * vertices.size();
@@ -100,7 +100,7 @@ Primitive::Primitive(Engine *engine, Mesh *mesh, const std::vector<Vertex> &vert
     createTextureResources();
 }
 
-Primitive::Primitive(Engine* engine, Mesh* mesh, tinygltf::Primitive tprimitive)
+Primitive::Primitive(Engine *engine, Mesh *mesh, tinygltf::Primitive tprimitive)
     : engine(engine),
       mesh(mesh),
       vertexBuffer(VK_NULL_HANDLE),
@@ -115,7 +115,7 @@ Primitive::Primitive(Engine* engine, Mesh* mesh, tinygltf::Primitive tprimitive)
       uniformBuffer(VK_NULL_HANDLE),
       uniformBufferMemory(VK_NULL_HANDLE),
       uniformBufferMapped(nullptr),
-      descriptorSet(VK_NULL_HANDLE)
+      primitiveDescriptorSet(VK_NULL_HANDLE)
 {
     // Initialize variables from tinygltf primitive
     if (tprimitive.attributes.count("POSITION") == 0 ||
@@ -127,7 +127,8 @@ Primitive::Primitive(Engine* engine, Mesh* mesh, tinygltf::Primitive tprimitive)
     }
 
     // Load POSITION, NORMAL, TEXCOORD_0
-    tinygltf::Model* tgltfModel = mesh->model->tgltfModel;
+    Model *model = mesh->model;
+    tinygltf::Model *tgltfModel = model->tgltfModel;
     const auto &posAccessor = tgltfModel->accessors[tprimitive.attributes.at("POSITION")];
     const auto &normAccessor = tgltfModel->accessors[tprimitive.attributes.at("NORMAL")];
     const auto &texAccessor = tgltfModel->accessors[tprimitive.attributes.at("TEXCOORD_0")];
@@ -198,11 +199,8 @@ Primitive::Primitive(Engine* engine, Mesh* mesh, tinygltf::Primitive tprimitive)
 
     vkMapMemory(engine->logicalDevice, uniformBufferMemory, 0, uboSize, 0, &uniformBufferMapped);
 
-    // Create texture resources
-    createTextureSampler();
-    createDefaultTexture();
-    createTextureImageView();
-    updateDescriptorSet();
+    // Create texture resources using consolidated initialization
+    createTextureResources();
 }
 
 Primitive::~Primitive()
@@ -230,9 +228,9 @@ Primitive::~Primitive()
     }
 
     // Free descriptor set if allocated
-    if (descriptorSet != VK_NULL_HANDLE)
+    if (primitiveDescriptorSet != VK_NULL_HANDLE)
     {
-        vkFreeDescriptorSets(engine->logicalDevice, engine->descriptorPool, 1, &descriptorSet);
+        vkFreeDescriptorSets(engine->logicalDevice, engine->descriptorPool, 1, &primitiveDescriptorSet);
     }
 
     // Destroy texture resources
@@ -258,51 +256,34 @@ Primitive::~Primitive()
 
 void Primitive::updateDescriptorSet()
 {
-    if (descriptorSet == VK_NULL_HANDLE)
+    if (primitiveDescriptorSet == VK_NULL_HANDLE)
     {
         throw std::runtime_error("Cannot update null descriptor set");
     }
 
     // Update UBO descriptor
-    VkDescriptorBufferInfo bufferInfo{};
     bufferInfo.buffer = uniformBuffer;
     bufferInfo.offset = 0;
     bufferInfo.range = sizeof(UniformBufferObject);
 
-    // Update texture descriptor if available
-    VkDescriptorImageInfo imageInfo{};
-    if (textureImageView != VK_NULL_HANDLE && textureSampler != VK_NULL_HANDLE)
-    {
-        imageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-        imageInfo.imageView = textureImageView;
-        imageInfo.sampler = textureSampler;
-    }
+    // Always update texture descriptor - createTextureResources() ensures we have valid defaults
+    imageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+    imageInfo.imageView = textureImageView;
+    imageInfo.sampler = textureSampler;
 
     std::array<VkWriteDescriptorSet, 2> descriptorWrites = {};
 
-    // UBO at binding 0
+    // Only update sampler binding (since this is a texture-only descriptor set)
     descriptorWrites[0].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-    descriptorWrites[0].dstSet = descriptorSet;
+    descriptorWrites[0].dstSet = primitiveDescriptorSet;
     descriptorWrites[0].dstBinding = 0;
     descriptorWrites[0].dstArrayElement = 0;
-    descriptorWrites[0].descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    descriptorWrites[0].descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
     descriptorWrites[0].descriptorCount = 1;
-    descriptorWrites[0].pBufferInfo = &bufferInfo;
+    descriptorWrites[0].pImageInfo = &imageInfo;
 
-    // Sampler at binding 1 (only if we have a texture)
-    if (textureImageView != VK_NULL_HANDLE && textureSampler != VK_NULL_HANDLE)
-    {
-        descriptorWrites[1].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-        descriptorWrites[1].dstSet = descriptorSet;
-        descriptorWrites[1].dstBinding = 1;
-        descriptorWrites[1].dstArrayElement = 0;
-        descriptorWrites[1].descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-        descriptorWrites[1].descriptorCount = 1;
-        descriptorWrites[1].pImageInfo = &imageInfo;
-    }
-
-    // Only update the texture descriptor if we have one
-    uint32_t writeCount = (textureImageView != VK_NULL_HANDLE && textureSampler != VK_NULL_HANDLE) ? 2 : 1;
+    // Always update both descriptors
+    uint32_t writeCount = 2;
     vkUpdateDescriptorSets(engine->logicalDevice, writeCount, descriptorWrites.data(), 0, nullptr);
 }
 
@@ -317,7 +298,6 @@ void Primitive::updateUniformBuffer(const glm::mat4 &model, const glm::mat4 &vie
 
 void Primitive::createTextureSampler()
 {
-    VkSamplerCreateInfo samplerInfo{};
     samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
     samplerInfo.magFilter = VK_FILTER_LINEAR;
     samplerInfo.minFilter = VK_FILTER_LINEAR;
@@ -346,15 +326,51 @@ void Primitive::createTextureSampler()
 
 void Primitive::createTextureResources()
 {
+    // Create texture resources first
     createTextureSampler();
     createDefaultTexture();
     createTextureImageView();
+
+    // Only proceed if we have valid texture resources
+    if (textureImageView == VK_NULL_HANDLE || textureSampler == VK_NULL_HANDLE)
+    {
+        throw std::runtime_error("Failed to create texture resources!");
+    }
+
+    // Create descriptor set layout for texture sampler
+    samplerLayoutBinding.binding = 0;
+    samplerLayoutBinding.descriptorCount = 1;
+    samplerLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+    samplerLayoutBinding.pImmutableSamplers = nullptr;
+    samplerLayoutBinding.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
+
+    textureLayoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+    textureLayoutInfo.bindingCount = 1;
+    textureLayoutInfo.pBindings = &samplerLayoutBinding;
+
+
+    if (vkCreateDescriptorSetLayout(engine->logicalDevice, &textureLayoutInfo, nullptr, &engine->textureDescriptorSetLayout) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to create texture descriptor set layout!");
+    }
+
+    // Allocate descriptor set for texture
+    allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+    allocInfo.descriptorPool = engine->descriptorPool;
+    allocInfo.descriptorSetCount = 1;
+    allocInfo.pSetLayouts = &engine->textureDescriptorSetLayout;
+
+    if (vkAllocateDescriptorSets(engine->logicalDevice, &allocInfo, &primitiveDescriptorSet) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to allocate texture descriptor set!");
+    }
+
+    // Clean up the layout after allocation
+    vkDestroyDescriptorSetLayout(engine->logicalDevice, engine->textureDescriptorSetLayout, nullptr);
+
     updateDescriptorSet();
 }
 
 void Primitive::createTextureImageView()
 {
-    VkImageViewCreateInfo viewInfo{};
     viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
     viewInfo.image = textureImage;
     viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
@@ -382,13 +398,12 @@ void Primitive::createDefaultTexture()
     VkBuffer stagingBuffer;
     VkDeviceMemory stagingBufferMemory;
 
-    VkBufferCreateInfo bufferInfo{};
-    bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-    bufferInfo.size = sizeof(pixel);
-    bufferInfo.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
-    bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    stagingBufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    stagingBufferInfo.size = sizeof(pixel);
+    stagingBufferInfo.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+    stagingBufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
 
-    if (vkCreateBuffer(engine->logicalDevice, &bufferInfo, nullptr, &stagingBuffer) != VK_SUCCESS)
+    if (vkCreateBuffer(engine->logicalDevice, &stagingBufferInfo, nullptr, &stagingBuffer) != VK_SUCCESS)
     {
         throw std::runtime_error("Failed to create staging buffer for default texture!");
     }
@@ -397,13 +412,12 @@ void Primitive::createDefaultTexture()
     VkMemoryRequirements memRequirements;
     vkGetBufferMemoryRequirements(engine->logicalDevice, stagingBuffer, &memRequirements);
 
-    VkMemoryAllocateInfo allocInfo{};
-    allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-    allocInfo.allocationSize = memRequirements.size;
-    allocInfo.memoryTypeIndex = engine->findMemoryType(memRequirements.memoryTypeBits,
+    stagingAllocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+    stagingAllocInfo.allocationSize = memRequirements.size;
+    stagingAllocInfo.memoryTypeIndex = engine->findMemoryType(memRequirements.memoryTypeBits,
                                                        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
 
-    if (vkAllocateMemory(engine->logicalDevice, &allocInfo, nullptr, &stagingBufferMemory) != VK_SUCCESS)
+    if (vkAllocateMemory(engine->logicalDevice, &stagingAllocInfo, nullptr, &stagingBufferMemory) != VK_SUCCESS)
     {
         throw std::runtime_error("Failed to allocate staging buffer memory!");
     }
@@ -417,22 +431,21 @@ void Primitive::createDefaultTexture()
     vkUnmapMemory(engine->logicalDevice, stagingBufferMemory);
 
     // Create texture image
-    VkImageCreateInfo imageInfo{};
-    imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
-    imageInfo.imageType = VK_IMAGE_TYPE_2D;
-    imageInfo.extent.width = width;
-    imageInfo.extent.height = height;
-    imageInfo.extent.depth = 1;
-    imageInfo.mipLevels = 1;
-    imageInfo.arrayLayers = 1;
-    imageInfo.format = VK_FORMAT_R8G8B8A8_SRGB;
-    imageInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
-    imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-    imageInfo.usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
-    imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-    imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+    textureImageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+    textureImageInfo.imageType = VK_IMAGE_TYPE_2D;
+    textureImageInfo.extent.width = width;
+    textureImageInfo.extent.height = height;
+    textureImageInfo.extent.depth = 1;
+    textureImageInfo.mipLevels = 1;
+    textureImageInfo.arrayLayers = 1;
+    textureImageInfo.format = VK_FORMAT_R8G8B8A8_SRGB;
+    textureImageInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
+    textureImageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    textureImageInfo.usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
+    textureImageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    textureImageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
 
-    if (vkCreateImage(engine->logicalDevice, &imageInfo, nullptr, &textureImage) != VK_SUCCESS)
+    if (vkCreateImage(engine->logicalDevice, &textureImageInfo, nullptr, &textureImage) != VK_SUCCESS)
     {
         throw std::runtime_error("Failed to create default texture image!");
     }
@@ -440,11 +453,11 @@ void Primitive::createDefaultTexture()
     // Allocate texture image memory
     vkGetImageMemoryRequirements(engine->logicalDevice, textureImage, &memRequirements);
 
-    allocInfo.allocationSize = memRequirements.size;
-    allocInfo.memoryTypeIndex = engine->findMemoryType(memRequirements.memoryTypeBits,
+    stagingAllocInfo.allocationSize = memRequirements.size;
+    stagingAllocInfo.memoryTypeIndex = engine->findMemoryType(memRequirements.memoryTypeBits,
                                                        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
 
-    if (vkAllocateMemory(engine->logicalDevice, &allocInfo, nullptr, &textureImageMemory) != VK_SUCCESS)
+    if (vkAllocateMemory(engine->logicalDevice, &stagingAllocInfo, nullptr, &textureImageMemory) != VK_SUCCESS)
     {
         throw std::runtime_error("Failed to allocate texture image memory!");
     }
@@ -453,21 +466,19 @@ void Primitive::createDefaultTexture()
 
     // Transition image layout for copying
     VkCommandBuffer cmdBuffer = engine->beginSingleTimeCommands();
-
-    VkImageMemoryBarrier barrier{};
-    barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
-    barrier.oldLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-    barrier.newLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
-    barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
-    barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
-    barrier.image = textureImage;
-    barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-    barrier.subresourceRange.baseMipLevel = 0;
-    barrier.subresourceRange.levelCount = 1;
-    barrier.subresourceRange.baseArrayLayer = 0;
-    barrier.subresourceRange.layerCount = 1;
-    barrier.srcAccessMask = 0;
-    barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+    imageBarrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+    imageBarrier.oldLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    imageBarrier.newLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
+    imageBarrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+    imageBarrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+    imageBarrier.image = textureImage;
+    imageBarrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+    imageBarrier.subresourceRange.baseMipLevel = 0;
+    imageBarrier.subresourceRange.levelCount = 1;
+    imageBarrier.subresourceRange.baseArrayLayer = 0;
+    imageBarrier.subresourceRange.layerCount = 1;
+    imageBarrier.srcAccessMask = 0;
+    imageBarrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
 
     vkCmdPipelineBarrier(
         cmdBuffer,
@@ -476,19 +487,18 @@ void Primitive::createDefaultTexture()
         0,
         0, nullptr,
         0, nullptr,
-        1, &barrier);
+        1, &imageBarrier);
 
     // Copy buffer to image
-    VkBufferImageCopy region{};
-    region.bufferOffset = 0;
-    region.bufferRowLength = 0;
-    region.bufferImageHeight = 0;
-    region.imageSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-    region.imageSubresource.mipLevel = 0;
-    region.imageSubresource.baseArrayLayer = 0;
-    region.imageSubresource.layerCount = 1;
-    region.imageOffset = {0, 0, 0};
-    region.imageExtent = {width, height, 1};
+    imageCopyRegion.bufferOffset = 0;
+    imageCopyRegion.bufferRowLength = 0;
+    imageCopyRegion.bufferImageHeight = 0;
+    imageCopyRegion.imageSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+    imageCopyRegion.imageSubresource.mipLevel = 0;
+    imageCopyRegion.imageSubresource.baseArrayLayer = 0;
+    imageCopyRegion.imageSubresource.layerCount = 1;
+    imageCopyRegion.imageOffset = {0, 0, 0};
+    imageCopyRegion.imageExtent = {width, height, 1};
 
     vkCmdCopyBufferToImage(
         cmdBuffer,
@@ -496,10 +506,9 @@ void Primitive::createDefaultTexture()
         textureImage,
         VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
         1,
-        &region);
+        &imageCopyRegion);
 
     // Transition image layout to shader read
-    VkImageMemoryBarrier shaderBarrier{};
     shaderBarrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
     shaderBarrier.oldLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
     shaderBarrier.newLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
@@ -529,38 +538,24 @@ void Primitive::createDefaultTexture()
     vkDestroyBuffer(engine->logicalDevice, stagingBuffer, nullptr);
     vkFreeMemory(engine->logicalDevice, stagingBufferMemory, nullptr);
 
-    // Update descriptor set with sampler
-    VkDescriptorImageInfo descImageInfo{};
-    descImageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-    descImageInfo.imageView = textureImageView;
-    descImageInfo.sampler = textureSampler;
-
-    VkWriteDescriptorSet descriptorWrite{};
-    descriptorWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-    descriptorWrite.dstSet = engine->descriptorSet;
-    descriptorWrite.dstBinding = 1;
-    descriptorWrite.dstArrayElement = 0;
-    descriptorWrite.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-    descriptorWrite.descriptorCount = 1;
-    descriptorWrite.pImageInfo = &descImageInfo;
-
-    vkUpdateDescriptorSets(engine->logicalDevice, 1, &descriptorWrite, 0, nullptr);
+    // Descriptor set will be updated by createTextureResources()
 }
 
 Mesh::Mesh(Engine *engine, Model *model, tinygltf::Mesh gltfmesh)
-    : engine(engine)
+    : engine(engine), model(model)
 {
     // Process all primitives in the mesh
     for (const auto &tprimitive : gltfmesh.primitives)
     {
         // Let the Primitive class handle the loading
-        primitives.emplace_back(Primitive(engine, this, tprimitive));
+        primitives.emplace_back(engine, this, tprimitive);
     }
 }
 
 Mesh::Mesh(Engine *engine, Model *model, const std::vector<Vertex> &vertices)
-    : engine(engine)
+    : engine(engine), model(model)
 {
+    primitives.emplace_back(engine, this, vertices);
 }
 
 Mesh::~Mesh()
@@ -570,7 +565,7 @@ Mesh::~Mesh()
 Model::Model(const std::vector<Vertex> &vertices, Engine *engine) : engine(engine)
 {
     // Create a single mesh from the provided vertices
-    meshes.emplace_back(Mesh(engine, this, vertices));
+    meshes.emplace_back(engine, this, vertices);
 }
 
 Model::Model(const std::string &gltfPath, Engine *engine) : engine(engine)
@@ -579,9 +574,10 @@ Model::Model(const std::string &gltfPath, Engine *engine) : engine(engine)
     tinygltf::TinyGLTF loader;
     std::string err, warn;
 
-    tgltfModel = new tinygltf::Model();
     bool success = false;
     const std::string ext = gltfPath.substr(gltfPath.find_last_of('.'));
+    // initialize the model
+    tgltfModel = new tinygltf::Model();
 
     if (ext == ".glb")
     {
@@ -609,7 +605,7 @@ Model::Model(const std::string &gltfPath, Engine *engine) : engine(engine)
     for (const auto &gltfmesh : tgltfModel->meshes)
     {
         // Create a new Mesh for each GLTF mesh
-        meshes.emplace_back(Mesh(engine, this, gltfmesh));
+        meshes.emplace_back(engine, this, gltfmesh);
     }
 }
 
@@ -625,8 +621,5 @@ Model::~Model()
     meshes.clear();
 
     // Clean up GLTF model
-    if (tgltfModel) {
-        delete tgltfModel;
-        tgltfModel = nullptr;
-    }
+    delete &tgltfModel;
 }
